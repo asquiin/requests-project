3c624069c32651603fd82253564ff2973198442f
Assel <assel.amerkanova@nu.edu.kz>
2024-11-25 17:04:44 +0500

    login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,6 +3,19 @@
 </script>
 
 <template>
+<div class="login-page w-full h-screen d-flex">
+<v-col > 
+<v-card variant="flat" color="white" width="400px">
+<v-text-field placeholder="Телефон" variant="flat"> </v-text-field>
+</v-card>
+</v-col>
+</div>
 
-    <h1> login</h1>
 </template>
+
+<style lang="scss" scoped>
+.login-page {
+  background: url('src/assets/authpic.jpeg') no-repeat center center;
+  background-size: cover;
+}
+</style>
