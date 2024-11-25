0715e6e7c4328f0e73c51fe1ff3060426810ebde
Assel <assel.amerkanova@nu.edu.kz>
2024-11-25 17:19:08 +0500

    login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,10 +2,20 @@
 
 <template>
   <div class="login-page w-full h-screen bg-red justify-center align-center">
-    <v-card variant="flat" color="white" width="400px">
+    <v-card variant="flat" color="white" width="400px" class="rounded-lg">
       <v-col>
+      <v-list-item class="mb-0 pb-0" :style="{color: '#50B053'}"> Логин или Телефон </v-list-item>
         <v-text-field placeholder="Телефон" variant="underlined" />
         <v-text-field placeholder="Пароль" variant="underlined" />
+        <v-card-actions class="flex justify-center">
+          <v-btn
+            text="Войти"
+            width="110px"
+            height="36px"
+            variant="flat"
+            color="#50B053"
+          />
+        </v-card-actions>
       </v-col>
     </v-card>
   </div>
@@ -16,5 +26,6 @@
   background: url("src/assets/authpic.jpeg") no-repeat center center;
   background-size: cover;
   display: flex;
+
 }
 </style>
